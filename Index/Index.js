b89d8e3977298993f14309eb25f1b32dc369d637
unknown <nicolasmargossian@gmail.com>
2018-12-26 01:11:57 -0300

    estructura html

---
+++ Index/Index.js
@@ -0,0 +1,28 @@
+function CosasGastiXD(){
+	console.log(e.edad)
+
+	$(document).ready(function(){
+		$('.texto').css('color', 'Green')
+		const test = 2
+		const test2 = 3
+		console.log(test + test2)
+	})
+	
+	const a = 1
+	const b = 'asd'
+	const c = true
+	const d = [1, 2, 3, 'asd', [0, 2]]
+	const e = {
+		edad: 21,
+		altura: '1.10m',
+		decirHola: function(){
+			console.log(e.edad)
+		}
+	}	
+	e.decirHola()
+}
+function CambioColor()	{
+$(document).ready(function(){
+	$('.texto').css('color', 'Green')
+})
+}
