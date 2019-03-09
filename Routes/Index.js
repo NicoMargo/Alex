9aeea1703351ca00bc00c8790d523745baf0c27d
Dolni <39602138+Dolni@users.noreply.github.com>
2019-03-09 20:09:55 -0300

    css arreglado

--- Routes/Index.js
+++ Routes/Index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res)=>{
-   res.send('Hello world');
+   res.send('Hello world martin');
 });
 module.exports = router;
