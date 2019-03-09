fbe80264b852d7c98ee0b247c7b9eddcd8e60719
unknown <nicolasmargossian@gmail.com>
2019-03-09 20:19:43 -0300

    motor server config

--- Index.js
+++ Index.js
@@ -8,14 +8,33 @@ function Acarrito(){
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 app.set('port', process.env.PORT || 4000 );
+app.engine('.hbs', exphbs({
+	defaultLayout: 'main',
+	layoutsdir: path.join(app.get('views'), 'layouts'),
+	partialdir: path.join(app.get('views'), 'partials'),
+	extname: '.hbs',
+	helpers: require('./JS/Handlebars')
+}));
+
+app.set('view engine', '.hbs');
+
+//Global Variables
+app.use((req, res, nest)=>{
+next();
+});
+//middlewars
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 app.use(morgan('dev'));
-
-
 
 //Routes
 app.use(require('./Routes/Index'));
+app.use(require('./Routes/Authentication'));
+app.use('/links',require('./Routes/Links'));
 // Public
 
 // Starting the server
