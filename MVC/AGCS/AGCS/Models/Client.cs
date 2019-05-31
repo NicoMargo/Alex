fad309672e995a2b3228a961d8fbdbe25a909ca7
NICOLAS ALEJANDRO MARGOSSIAN <43994080@ort>
2019-05-31 08:56:56 -0300

    a

---
+++ MVC/AGCS/AGCS/Models/Client.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AGCS.Models
+{
+    public class Client
+    {
+        private int _id;
+        private string _name;
+        private string _surname;
+        private int _dni;
+        private string _email;
+        private int _phone;
+        private int _cellphone;
+        private string _town;
+        private string _Address;
+        private string _province;
+        private string _leter;
+        private int _number;
+        private int _floor;
+
+        //Only a few data for ABM client
+        public Client(int id, string name, string surname, int dni, string email, int cellphone)
+        {
+            _id = id;
+            _name = name;
+            _surname = surname;
+            _dni = dni;
+            _email = email;
+            _cellphone = cellphone;
+        }
+
+        //Full Client
+        public Client(int id, string Name, string Surname, int dni, string email, int Telephone, int Cellphone, string Town, string Address, string Province, string Leter, int Number, int Floor)
+        {
+            _id = id;
+            _name = Name;
+            _surname = Surname;
+            _dni = dni;
+            _email = email;
+            _phone = Telephone;
+            _cellphone = Cellphone;
+            _town = Town;
+            _Address = Address;
+            _province = Province;
+            _leter = Leter;
+            _number = Number;
+            _floor = Floor;
+        }
+        //New Client
+        public Client( string Name, string Surname, int dni, string email, int Telephone, int Cellphone, string Town, string Address, string Province, string Leter, int Number, int Floor)
+        {
+            _name = Name;
+            _surname = Surname;
+            _dni = dni;
+            _email = email;
+            _phone = Telephone;
+            _cellphone = Cellphone;
+            _town = Town;
+            _Address = Address;
+            _province = Province;
+            _leter = Leter;
+            _number = Number;
+            _floor = Floor;
+        }
+
+        public int Id { get => _id; }
+        public string Name { get => _name; }
+        public string Surname { get => _surname; }
+        public int Dni { get => _dni; }
+        public string Email { get => _email; }
+        public int Telephone { get => _phone; }
+        public int Cellphone { get => _cellphone; }
+        public string Town { get => _town; }
+        public string Address { get => _Address; }
+        public string Province { get => _province; }
+        public string Leter { get => _leter; }
+        public int Number { get => _number; }
+        public int Floor { get => _floor; }
+    }
+}
---
+++ MVC/AGCS/AGCS/wwwroot/js/ButtonIDClick.js
@@ -0,0 +1,106 @@
+﻿$(document).ready(function () {
+    $(".imageABM").click(function () {
+        let Index = $(this).attr("position");
+        $.ajax({
+            type: "POST",
+            url: urlGetOne,
+            data: { pos: Index },
+            success: function (DataJsonClient) {
+                var Data = JSON.parse(DataJsonClient);
+                $("#modalSurname").val(Data.Surname);
+                $("#modalName").val(Data.Name);
+                $("#modalDni").val(Data.Dni);
+                $("#modelEmail").val(Data.Email);
+                $("#modalTelephone").val(Data.Telephone);
+                $("#modalCellphone").val(Data.Cellphone);
+                $("#modelTown").val(Data.Town);
+                $("#modelAddress").val(Data.Address);
+                $("#modelAppartment").val(Data.Leter);
+                $("#modelNumber").val(Data.Number);
+                $("#modelFloor").val(Data.Floor);
+            },
+            error: function () {
+                alert("ERROR");
+            }
+        });
+    });
+
+
+    $("#Submit").click(function () {
+        $.ajax({
+            type: "POST",
+            url: urlUpdate,
+            data: {
+                Surname: $("#modalSurname").val(),
+                Name: $("#modalName").val(),
+                dni: $("#modalDni").val(),
+                email: $("#modelEmail").val(),
+                Telephone: $("#modalTelephone").val(),
+                Cellphone: $("#modalCellphone").val(),
+                Town: $("#modelTown").val(),
+                Address: $("#modelAddress").val(),
+                Province: 1,
+                Leter: $("#modelAppartment").val(),
+                Number: $("#modelNumber").val(),
+                Floor: $("#modelFloor").val()
+            },
+            success: function () {
+                location.reload();
+            },
+            error: function () {
+                alert("ERROR");
+            }
+        });
+    });
+
+    $(".w-50").click(function () {
+        let Index = $(this).attr("position");
+         $("#confirm").click(function () {
+            $.ajax({
+                type: "DELETE",
+                url: urlDelete,
+                data: { id: Index },
+                success: function () {
+                    location.reload();
+                },
+                error: function () {
+                    alert("ERROR");
+                }
+            });
+        });
+    });
+
+    $("#newClient").click(function () {
+        if ($("#modalCreateName").val() !== "" && $("#modalCreateName").val() !== "" && $("#modalCreateDni").val() !== 0) {
+            $.ajax({
+                type: "POST",
+                url: urlCreate,
+                data: {
+                    Surname: $("#modalCreateSurname").val(),
+                    Name: $("#modalCreateName").val(),
+                    dni: $("#modalCreateDni").val(),
+                    email: $("#modalCreateEmail").val(),
+                    Telephone: $("#modalCreateTelephone").val(),
+                    Cellphone: $("#modalCreateCellphone").val(),
+                    Town: $("#modalCreateTown").val(),
+                    Address: $("#modalCreateAddressC").val(),
+                    Province: 1,
+                    Leter: $("#modelCreateAppartment").val(),
+                    Number: $("#modelCreateNumber").val(),
+                    Floor: $("#modelCreateFloor").val()
+                },
+                success: function () {
+                    location.reload();
+                },
+                error: function () {
+                    alert("ERROR");
+                }
+            });
+        }
+        else {
+            if ($("#modalCreateName").val() === "") {
+                $("#modalCreateName").addClass("validation_error");
+            }
+        }
+    });
+});
